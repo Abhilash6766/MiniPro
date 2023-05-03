33d43b243fb389e5c8f58710f8ccb9fe0b6a4df0
20b81a1204 <124117621+20b81a1204@users.noreply.github.com>
2023-05-03 18:28:35 +0530

    Create loginAdmin.js
    loginAdmin

---
+++ loginAdmin.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+
+function LoginPage() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    // TODO: Add code to validate username and password and handle login logic
+  };
+
+  return (
+    <div>
+      <h1> </h1>
+      <h1>Login Admin</h1>
+      <form onSubmit={handleFormSubmit}>
+        <label>
+          Username:
+          <input type="text" value={username} onChange={handleUsernameChange} />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input type="password" value={password} onChange={handlePasswordChange} />
+        </label>
+        <br />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default LoginPage;
